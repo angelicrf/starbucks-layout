74b64c75dc096fe6c68573a50aff73dc4e627f51
Your Name <angelicrf@gmail.com>
2021-11-28 13:48:19 -0800

    navbar-colors

---
+++ index.js
@@ -0,0 +1,15 @@
+const changeLisColor = (thisClass, thisEvent, thisColor) => {
+  document.querySelectorAll(thisClass).forEach((item) => {
+    item.addEventListener(thisEvent, function (event) {
+      event.target.style.color = thisColor;
+      console.log("hovered");
+    });
+  });
+};
+function changeBtnColors() {
+  changeLisColor(".changeUl", "mouseover", "red");
+  changeLisColor(".changeUl", "mouseout", "black");
+  changeLisColor(".signIn", "mouseout", "purple");
+  changeLisColor(".signOut", "mouseout", "pink");
+}
+changeBtnColors();
